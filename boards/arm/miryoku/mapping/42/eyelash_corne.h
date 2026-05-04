c719df8b981f04c9b8d945398e2cfaecdc7538e5
zhanggaofan <zhanggaofan0827@gmail.com>
2026-05-04 22:11:35 +0800

    Debug layout

--- boards/arm/miryoku/mapping/42/eyelash_corne.h
+++ boards/arm/miryoku/mapping/42/eyelash_corne.h
@@ -11,9 +11,14 @@
      K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, \
      N30, N31, K32, K33, K34,      K35, K36, K37, N38, N39 \
 ) \
-XXX  K00  K01  K02  K03  K04           J0        K05  K06  K07  K08  K09  XXX \
-XXX  K10  K11  K12  K13  K14        J1 J4 J2     K15  K16  K17  K18  K19  XXX \
-XXX  K20  K21  K22  K23  K24    E0     J3        K25  K26  K27  K28  K29  XXX \
+// XXX  K00  K01  K02  K03  K04           J0        K05  K06  K07  K08  K09  XXX \
+// XXX  K10  K11  K12  K13  K14        J1 J4 J2     K15  K16  K17  K18  K19  XXX \
+// XXX  K20  K21  K22  K23  K24    E0     J3        K25  K26  K27  K28  K29  XXX \
+//                K32  K33  K34                       K35  K36  K37
+
+XXX  K00  K01  K02  K03  K04           XXX        K05  K06  K07  K08  K09  XXX \
+XXX  K10  K11  K12  K13  K14        XXX XXX XXX     K15  K16  K17  K18  K19  XXX \
+XXX  K20  K21  K22  K23  K24    XXX     XXX        K25  K26  K27  K28  K29  XXX \
                K32  K33  K34                       K35  K36  K37
 
 #define MIRYOKU_KLUDGE_TOPROWCOMBOS_LEFTPINKIE 2 3
