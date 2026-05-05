45826dceab99463decbe1c76934c396114d92f88
zhanggaofan <zhanggaofan0827@gmail.com>
2026-05-05 15:27:46 +0800

    Try fix draw

--- boards/arm/miryoku/custom_config.h
+++ boards/arm/miryoku/custom_config.h
@@ -4,3 +4,18 @@
 #define MIRYOKU_ALPHAS_QWERTY
 
 #define MIRYOKU_NAV_VI
+
+/* Base 层中间三个键 */
+#define EYELASH_E04_BASE &kp LEFT
+#define EYELASH_E05_BASE &kp DOWN
+#define EYELASH_E06_BASE &kp UP
+
+/* Nav 层中间三个键 */
+#define EYELASH_E04_NAV &kp HOME
+#define EYELASH_E05_NAV &kp PG_DN
+#define EYELASH_E06_NAV &kp PG_UP
+
+/* Num 层中间三个键 */
+#define EYELASH_E04_NUM &kp N1
+#define EYELASH_E05_NUM &kp N2
+#define EYELASH_E06_NUM &kp N3
