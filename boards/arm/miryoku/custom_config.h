f6c75d08fd8032b5d2a66fdbc64f6d0bfa96b56e
zhanggaofan <zhanggaofan0827@gmail.com>
2026-05-05 16:08:19 +0800

    Support joy mouse

--- boards/arm/miryoku/custom_config.h
+++ boards/arm/miryoku/custom_config.h
@@ -6,14 +6,26 @@
 #define MIRYOKU_NAV_VI
 
 /* Base 层中间三个键 */
-#define EYELASH_E04_BASE &kp LEFT
-#define EYELASH_E05_BASE &kp DOWN
-#define EYELASH_E06_BASE &kp UP
+#define EYELASH_E00_BASE &mmv MOVE_UP
+#define EYELASH_E01_BASE &mmv MOVE_LEFT
+// #define EYELASH_E02_BASE 
+#define EYELASH_E03_BASE &mmv MOVE_RIGHT
+// #define EYELASH_E04_BASE &mmv
+#define EYELASH_E05_BASE &mmv MOVE_DOWN
+
+/* Base 层中间三个键 */
+#define EYELASH_E00_MOUSE &mmv MOVE_UP
+#define EYELASH_E01_MOUSE &mmv MOVE_LEFT
+#define EYELASH_E03_MOUSE &mmv MOVE_RIGHT
+#define EYELASH_E05_MOUSE &mmv MOVE_DOWN
 
 /* Nav 层中间三个键 */
-#define EYELASH_E04_NAV &kp HOME
+#define EYELASH_E00_NAV &kp PG_UP
+#define EYELASH_E01_NAV &kp HOME
+// #define EYELASH_E02_NAV &kp PG_UP
+#define EYELASH_E03_NAV &kp END
+// #define EYELASH_E04_NAV &kp PG_UP
 #define EYELASH_E05_NAV &kp PG_DN
-#define EYELASH_E06_NAV &kp PG_UP
 
 /* Num 层中间三个键 */
 #define EYELASH_E04_NUM &kp N1
